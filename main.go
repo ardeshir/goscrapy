52c0aa59984dee92a7a1156ccc1341c494eea001
Ardeshir Sepahsalar <ardeshir.org@gmail.com>
2023-07-01 12:37:00 -0500

    adding code

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "os"
+)
+
+func main() {
+
+  // create the variables for the response & error
+  var r *http.Response
+  var err error
+
+  // Request index.html from ardeshir.io
+  r, err = http.Get("https://www.ardeshir.io/index.html")
+
+  // if there is a problem accessing the server, kill the program and print the error to console
+    if err != nil {
+    panic(err) 
+    }
+
+  // Check the status code returned by the server
+  if r.StatusCode == 200 {
+   // The request was successful! 
+   var page []byte
+
+   // we know the size of the respon is 1270
+   var bodyLen int = 2540
+
+   page = make([]byte, bodyLen)
+
+  // read the data from server
+  r.Body.Read(page)
+   
+  // Open a writable file on your computer (create if it does not exits) 
+  var out *os.File 
+
+  out, err = os.OpenFile("index.html", os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+      panic(err)
+    }
+
+  // write the content 
+  out.Write(page) 
+  out.Close()
+
+  } else {
+    log.Fatal("Failed to retrieve the web page. Recieved status code ", r.Status)
+  } 
+  
+
+} // end of main
